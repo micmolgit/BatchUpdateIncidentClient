666da3421f49d5d41638a9fcb32b5a869697bb7e
Michel Molongo <mic.mol@outlook.com>
2018-06-12 14:53:44 +0400

    created dotnet core .sln and .csporj

---
+++ BatchUpdateIncidentClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BatchUpdateIncidentClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
